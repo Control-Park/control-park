561583b0b487317566c1eb36baf02f81f504f9ea
MangGlang <71730799+MangGlang@users.noreply.github.com>
2026-02-25 17:10:52 -0800

    feat: implement custom button component

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+interface CustomButtonProps {
+  title: string;
+  color?: string;
+  className?: string;
+  onPress?: () => void;
+}
+
+export default function CustomButton({
+  title = "",
+  color = "",
+  className = "",
+  onPress = () => {},
+}: CustomButtonProps) {
+  return (
+    <TouchableOpacity
+      className={`p-4 rounded-lg ${className}`}
+      style={{ backgroundColor: color }}
+      onPress={onPress}
+    >
+      <Text className="text-black text-md font-bold">{title}</Text>
+    </TouchableOpacity>
+  );
+}
