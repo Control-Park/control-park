bdfcabe38f0b79663525450827f1d8bee0b5b117
htnguyen622 <htnguyen622@gmail.com>
2026-02-28 16:31:43 -0800

    add SplashScreen.tsx

--- App.tsx
+++ App.tsx
@@ -2,14 +2,23 @@ import "./global.css";
 import { View, Text, StatusBar } from "react-native";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import SplashScreen from "./src/screens/SplashScreen"
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSplash(false);
+    }, 3000);
+  });
+  
   return (
     <View className="flex-1 bg-gray-300">
       <StatusBar barStyle="dark-content" />
-      {/* TODO: Build frontend component for splash screen */}
-      {/* <SplashScreen />  */}
-      <HomeScreen />
+      
+      <>{isShowSplash ? <SplashScreen /> : <HomeScreen />}</>
       {/* <SignUpScreen /> */}
     </View>
   );
