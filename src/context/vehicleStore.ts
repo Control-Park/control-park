7bb6f496d269774ef8ba63ee2f047c25b0f12abf
Nineiota0 <89941342+Nineiota0@users.noreply.github.com>
2026-04-13 00:54:12 -0700

    feat: add valdiation to vehicle modal form and save added vehicles to store

--- src/context/vehicleStore.ts
+++ src/context/vehicleStore.ts
@@ -1,12 +1,39 @@
 import { create } from "zustand";
-import { Vehicle } from "../data/mockVehicles";
+
+export type Vehicle = {
+  id: string;
+  name: string;
+  plate: string;
+  image?: string;
+  make: string;
+  model: string;
+  year: string;
+  color: string;
+};
 
 type VehicleStore = {
+  vehicles: Vehicle[];
   selectedVehicle: Vehicle | null;
   setSelectedVehicle: (vehicle: Vehicle | null) => void;
+  addVehicle: (vehicle: Vehicle) => void;
+  removeVehicle: (vehicleId: string) => void;
 };
 
 export const useVehicleStore = create<VehicleStore>((set) => ({
+  vehicles: [],
   selectedVehicle: null,
+
   setSelectedVehicle: (vehicle) => set({ selectedVehicle: vehicle }),
+
+  addVehicle: (vehicle) =>
+    set((state) => ({
+      vehicles: [...state.vehicles, vehicle],
+    })),
+
+  removeVehicle: (vehicleId) =>
+    set((state) => ({
+      vehicles: state.vehicles.filter((vehicle) => vehicle.id !== vehicleId),
+      selectedVehicle:
+        state.selectedVehicle?.id === vehicleId ? null : state.selectedVehicle,
+    })),
 }));
